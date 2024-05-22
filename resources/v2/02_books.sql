9d7b8db665bd0fff8f32c79aec01ad71113f6250
Rituraj Ingale <rituraj@wednesday.is>
2024-05-22 12:58:34 +0530

    Feat: Migration directory changed as per migration flow

--- resources/v2/02_books.sql
+++ resources/v2/02_books.sql
@@ -5,5 +5,5 @@ CREATE TABLE books (
     published_date   DATE,
     created_at      DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, 
     author_id        INT,
-    CONSTRAINT books_author_id_fk FOREIGN KEY (author_id) REFERENCES authors (id)  ON UPDATE CASCADE
+    CONSTRAINT books_author_id_fk FOREIGN KEY (author_id) REFERENCES authors (id) ON UPDATE CASCADE
 );
